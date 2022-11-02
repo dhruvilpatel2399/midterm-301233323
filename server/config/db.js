a474082cc0d048ec3e4e10eb570c0dc79b046b2c
dhruvil patel <dp3097@gmail.com>
2022-11-02 16:45:57 -0400

    initial commit

---
+++ server/config/db.js
@@ -0,0 +1,6 @@
+module.exports = {
+  //local MongoDB deployment ->
+  // "URI": "mongodb://localhost/books229"
+  "URI":"mongodb+srv://dhruvil:lNJJhuFfX54mcm9H@cluster0.w8w9o6h.mongodb.net/?retryWrites=true&w=majority"
+  // "URI":"mongodb+srv://cencollege:6GX6OmfUtvvclKT4@cluster0.vikkbvp.mongodb.net/?retryWrites=true&w=majority"
+};
